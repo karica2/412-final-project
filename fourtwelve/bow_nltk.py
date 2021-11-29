c3f09c914b6cea6e58d26abda4a26d29aa570d3a
Rob Schmidt <rwschmidt26@gmail.com>
2021-11-28 19:57:26 -0600

    Self-made TF-IDF implementation working
    Just need to add Guassian prediction now

--- fourtwelve/bow_nltk.py
+++ fourtwelve/bow_nltk.py
@@ -9,6 +9,7 @@ nltk.download('stopwords')
 ```
 Author:
 - Kenan Arica [karica2@uic.edu]
+- Rob Schmidt <rschmi2@uic.edu>
 """
 
 import collections
@@ -19,8 +20,7 @@ import numpy as np
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 
-# can just use tf-idf from here
-from nltk.text import TextCollection
+from nltk.text import TextCollection  # can just use tf-idf from here
 
 from .sanitizer import CommentSanitizer
 
@@ -44,14 +44,24 @@ of the powerful text filtering and compuational methods provided by NLTK, so thi
 
 class BagOfWords_NLTK:
 
-    def __init__(self, corpi: list) -> None:
+    def __init__(self, corpi: 'list[str]') -> None:
+        """
+        Create a BagOfWords implementation using NLTK
+
+        Args:
+        - `corpi`: list[str] - list of filenames to load as corpi
+        """
 
         raw_words = self.load_corpus(corpi)
         data = self.standardize_data(raw_words)
 
+        # save the comments data
+        # and for convenience save all comments in a list
         self._comments = data
         self._all_comments = [c['CONTENT'] for c in self._comments]
 
+        # TF-IDF stuff doesn't need to be done in the ctor, but
+        # this helped debugging at the cost of being a tad bit slower
         self._calculate_all_tf(data)
         self._calculate_idf(data)
 
@@ -112,6 +122,8 @@ class BagOfWords_NLTK:
             for link in links:
                 comment = comment.replace(link, LINK_CONSTANT)
 
+            # make the content portion of our new_words entry the sanitized comment
+            # we just defined above
             entry['CONTENT'] = comment
 
             """ now that we're done removing punctuation from our strings, we can make the words into tokens and replace stopwords using that functionality
@@ -121,13 +133,18 @@ class BagOfWords_NLTK:
                 we also don't need the comments in string form after we're done cleaning them -- they'll be tokenized down the line anyways
             """
 
+
+            # tokenize the comment and get rid of stopwords
             comment_tokenized = word_tokenize(comment)
-            # now we go through each word of the entry to look for stopwords
+            # can represent the tokens and stopwords as two sets and take the difference for speed
             comment_tokenized = np.setdiff1d(comment_tokenized, stopwords.words('english'))
 
             # for now, we're going to skip stemming and lemmatization
+            # TODO: Stemm and Lemm
             comment_tokenized = self.lemma_and_stem(comment_tokenized)
 
+            # new_words represents the CommentSanitizer dictionary, but has a new key called
+            # 'TOKENS' which represents the tokenized message content
             entry['TOKENS'] = comment_tokenized
             new_words.append(entry)
 
@@ -135,19 +152,20 @@ class BagOfWords_NLTK:
         self.n_ham_comments = meat_totals[0]
         self.n_spam_comments = meat_totals[1]
 
+        # return the new_words list which contains the sanitized comments and their tokenized forms
         return new_words
 
-    def remove_oneoffs(self) -> None:
-
-        removal_list = []
-
-        for entry in self.table:
-            if self.table[entry] == [0, 1] or self.table[entry] == [1, 0]:
-                removal_list.append(entry)
-
-        # remove them all (can't do in the first loop because of a RuntimeError: dictionary changed size during iteration thingy )
-        for entry in removal_list:
-            del self.table[entry]
+    # TODO: determine if this is actually necessary, TF-IDF should account for one-offs
+#    def remove_oneoffs(self) -> None:
+#        removal_list = []
+#
+#        for entry in self.table:
+#            if self.table[entry] == [0, 1] or self.table[entry] == [1, 0]:
+#                removal_list.append(entry)
+#
+#        # remove them all (can't do in the first loop because of a RuntimeError: dictionary changed size during iteration thingy )
+#        for entry in removal_list:
+#            del self.table[entry]
 
     def _calculate_tf(self, words: 'list[str]') -> 'dict[str, int]':
         """
@@ -199,52 +217,64 @@ class BagOfWords_NLTK:
         - None
         """
 
+        # combine all comment tokens into one big list
         all_words = []
         for comment in comments:
             all_words.extend(comment['TOKENS'])
 
+        # set some convenient instance variables
         self._idf_table = {}
         self._num_documents = len(comments)
         self._unique_words = set(all_words)
 
+        # calculate IDF for each word
         for word in self._unique_words:
-            # calculate IDF for each word
+            # creates a list of flags for if a word exists in a document
+            # the length of that list is the amount of instances of a word in a document
             instances = len([True for w in self._all_comments if word in w])
+            # do IDF math with smoothing
             self._idf_table[word] = np.log((self._num_documents + 1) / (instances + 1)) + 1
 
     def predict(self, comment: str) -> int:
+        """
+        Predict a comment's meat quality using a Guassian Naive Bayes model
+        """
 
-            # go through every word and get the probability
-            ham = 1
-            spam = 1
+        # TODO: implement model
+        pass
 
-            comment = self.standardize_data([[comment, -1]])[0]
+    def _calculate_tf_idf_matrix(self):
+        """
+        Perform a TF-IDF analysis on the data and classify the result using a Guassian Naive Bayes Model
 
-            for token in comment[0]:
+        Returns:
+        - None
+        """
 
-                # get both frequencies
+        self._tfidf = {}
+        for i in range(len(self._comments)):
+            for word in self._comments[i]['TOKENS']:
+                if word not in self._tfidf:
+                    self._tfidf[word] = 0
+                self._tfidf[word] += self._frequency_table[self._comments[i]['CONTENT']][word] * self._idf_table[word]
 
-                # not sure what to do here. If the token isn't in the table, do nothing? need input
-                if token not in self.table:
-                    continue
+    def _nltk_tf_idf(self) -> None:
+        """
+        Use NLTK TextCollection to perform TF-IDF
+        """
+        c = [c['CONTENT'] for c in self._comments]
 
-                entry = self.table[token]
-                num_ham = entry[0]
-                num_spam = entry[1]
+        t = TextCollection(c)
+        tfidf = {}
+        for i in range(len(self._comments)):
+            for word in self._comments[i]['TOKENS']:
+                if word not in tfidf:
+                    tfidf[word] = 0
+                tfidf[word] += t.tf_idf(word, self._comments[i]['CONTENT'])
 
-                # Get the term frequency
-                ham *= (num_ham + 1) / (self.n_ham_comments + 1)
-                spam *= (num_spam + 1) / (self.n_spam_comments + 1)
-
-            if ham > spam:
-                return 0
-            if spam > ham:
-                return 1
-
-
+        return tfidf
 
     def test_x_comments(self, filepath: str, num_comments: int, use_tf_idf: bool = False) -> None:
-
         CMS = CommentSanitizer(filepath)
         comments = CMS.parse()
         if num_comments > len(comments):
@@ -268,30 +298,3 @@ class BagOfWords_NLTK:
 
         logger.info(f"Number correct: {num_correct}/{num_comments}\t\t{round(num_correct / num_comments, 3)}%")
 
-
-    def _calculate_tf_idf_matrix(self):
-        """
-        Perform a TF-IDF analysis on the data and classify the result using a Guassian Naive Bayes Model
-
-        Returns:
-        - None
-        """
-
-        print(self._frequency_table.keys() == self._idf_table.keys())
-
-    def _nltk_tf_idf(self, comments: 'list[dict[str, str]]') -> None:
-        """
-        Use NLTK TextCollection to perform TF-IDF
-
-        Args:
-        - `comments`: list[dict[str, str]] - list of comments as parsed by a sanitizer
-        """
-        c = [c['CONTENT'] for c in comments]
-
-        t = TextCollection(c)
-        tfidf = {}
-        for i in range(len(comments)):
-            for word in comments[i]['TOKENS']:
-                tfidf[word] = t.tf_idf(word, comments[i]['CONTENT'])
-
-        return tfidf
