6b3b397ec9299ab92aa0a272d6d1bdc6962c8e2f
Kenan <karica2@uic.edu>
2021-11-30 21:22:58 -0600

    Created manual testing harness, and updated manual to include all toggle-able options. it still looks like dog water, though. but it works. I also made a sheets doc with the test output and shared it with you.

--- fourtwelve/bow_manual.py
+++ fourtwelve/bow_manual.py
@@ -11,154 +11,212 @@ import logging
 from .sanitizer import CommentSanitizer
 import re
 
+from nltk.stem.porter import PorterStemmer
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
 logger = logging.getLogger(__name__)
 
 LINK_CONSTANT = "LINKTOSITE"
 
 class BagOfWords_manual:
 
-    def __init__(self, filename: str) -> None:
+    def __init__(self, corpi: list, stemming: bool = False, remove_stopwords: bool = True, remove_oneoffs: bool = True) -> None:
         
-        self.CMS = CommentSanitizer(filename)
-        self.comments = self.CMS.parse()
+        self.load_corpus(corpi)
 
-    def get_frequency_table(self) -> None:
+        self.data = self.standardize_data(self.raw_words, stemming, remove_stopwords)
+        self.build_frequency_table(self.data)
+        if remove_oneoffs:
+            self.remove_oneoffs()
+
+    def load_corpus(self, corpi: list) -> None:
         """
-        1. Figure out what type of parameter we need (probably parsed csv, i don't think a numpy array is necessary)
-        2. Preprocess the data by getting rid of stop words and replacing links with one master word that indicates a link 
-        3. For each word in each comment, update our internal table with a counter as to whether it's from spam or ham
+            load a list of strings pointing to corpus files and concatenate them all, that can be accessed through the 'words' variable 
+            
+            Args:
+            - `corpi`: list[str] - A list of paths to corpus files  
 
-        NOTE: This method allows preprocessing to happen before this function is called, so any stop word removal or link parsing doesn't have to happen here
-
-        internal dict layout: 
-            key: [word] value: [tuple(# of occurances in ham, # of occurances in spam)]
-
-        NOTE: class 0: ham, class 1:spam
+            Returns:
+            - None        
         """
-        table = {}
 
-        # go through 
-        for comment in self.comments:
+        master_table = []
 
-            # split our comment into words
-            words = comment["CONTENT"].split()
-            spam_class = int(comment["CLASS"])
-            # for each word, make or update our entry in the table
-            for token in words:
+        for corpus in corpi:
 
-                token = token.lower()
-                # if it's not there already, make an entry 
-                if token not in table:
-                    if spam_class == 0:
-                        table[token] = [1, 0]
-                    if spam_class == 1:
-                        table[token] = [0, 1]
+            # take our collection of strings, make a parser from each
+            CMS = CommentSanitizer(corpus)
+            master_table += CMS.parse()
 
-                else:
-                    table[token][spam_class] += 1
-        # logger.info(table)
-        self._table = table
+        # we don't actually need most of the information given to us -- just the comment and meat value
+        self.raw_words = [[comment["CONTENT"], int(comment["CLASS"])] for comment in master_table]
 
-    def replace_link_with_constant(self) -> dict:
+
+    def stem(self, content: list) -> list:
         """
-        Replace a link in a comment with the LINK_CONSTANT
+            for each word in content, stem it if it can be stemmed 
+            
+            Args:
+            - `content`: list[str] - A list of words  
 
-        This function will replace multiple occurrances of a link as well
+            Returns:
+            - list[str]        
+        """
+
+        # we actually don't need to lemmatize the words because NB doesn't care about context 
+        porter = PorterStemmer()
+        ported = []
+        for word in content:
+            ported.append(porter.stem(word))
+
+        return ported
+        
+
+
+    def standardize_data(self, words: list, stemming: bool = False, remove_stopwords: bool = True) -> list:
+        """
+            load a list of strings pointing to corpus files and concatenate them all, that can be accessed through the 'words' variable 
+            
+            Args:
+            - `words`: list[] - a list of lists in the format [string, meat class (int)]
+            - `stemming`: bool = False - whether or not to use stemming
+
+            Returns:
+            - list of standardized words        
         """
         
-        for idx, comment in enumerate(self.comments):
-            content = comment["CONTENT"]
-            links = re.findall(r'(https?://[^\s]+)', content)
+        # while we cycle through the data we may as well keep track of how much ham / spam we have 
+        meat_totals = [0, 0]
+        new_words = []
+        for entry in words: 
+
+            comment = entry[0]
+            meat_totals[entry[1]] += 1
+
+            # lowercase the data 
+            comment = comment.lower()
            
             # for each link, replace it with our constant
+            links = re.findall(r'(https?://[^\s]+)', comment)
+            comment = re.sub("!|\?|\.|/|,", " ", comment)
             for link in links:
-                content = content.replace(link, LINK_CONSTANT)
+                comment = comment.replace(link, LINK_CONSTANT)
 
-            # strip out any symbols
-            content = re.sub("!|\?|\.|/|,", " ", content)
-            self.comments[idx]["CONTENT"] = content
+            # break up the content, remove stopwords
+            comment_tokenized = word_tokenize(comment)
+            if remove_stopwords:
 
-        pass
-    
-    def print_comments(self): 
+                for word in comment_tokenized:
+                    if word in stopwords.words('english'):
+                        # remove it
+                        comment_tokenized.remove(word)
+
+            if stemming == True:
+                comment_tokenized = self.stem(comment_tokenized)
+            new_words.append([comment_tokenized, entry[1]])
+
+        self.n_ham = meat_totals[0]
+        self.n_spam = meat_totals[1]
+
+        return new_words
+
+    # def sanitize_comment(comment: str) -> list
+
+
+    def remove_oneoffs(self) -> None:
         """
-        Log the comments to stdout
+            remove all words that only occur once in only one class
+        
+            Returns:
+            - None      
         """
+        removal_list = []
 
-        for comment in self.comments:
-            logger.info(comment)
-    
+        for entry in self.table: 
+            if self.table[entry] == [0, 1] or self.table[entry] == [1, 0]:
+                removal_list.append(entry)
 
-    # TODO: just call this function from replace_link_with_constant
+        for entry in removal_list:
+            del self.table[entry]
 
-    def clean_test_string(self, input_str: str) -> str:
+    def build_frequency_table(self, data: list) -> None:
+        self.table = {}
+        self.total_words_ham = 0
+        self.total_words_spam = 0
+        for comment in data:
 
-        links = re.findall(r'(https?://[^\s]+)', input_str)
-           
-        # for each link, replace it with our constant
-        for link in links:
+            # get the class 
+            meat_type = comment[1]
+            # 
+            for word in comment[0]: 
 
-            input_str = input_str.replace(link, LINK_CONSTANT)
-
-        input_string = input_str.lower()
-        # strip out any symbols
-        content = re.sub("!|\?|\.|/|,", " ", input_string)
-
-        return content
+                if word not in self.table:
+                    if meat_type == 0:
+                        self.total_words_ham += 1
+                        self.table[word] = [1, 0]
+                    elif meat_type == 1:
+                        self.total_words_spam += 1
+                        self.table[word] = [0, 1]
+                else:
+                    self.table[word][meat_type] += 1
+        
+    # pulled these functions from bow_manual like an idiot and modded it so i could see if the original method of prediction was any better using this implementation
+    # it's not.
 
     def predict(self, comment: str) -> int:
-        """
-        Predict the spamminess or hamminess of a comment
-        and return the comment class
 
-        Args:
-        - comment: str - comment string to evaluate
+            # go through every word and get the probability
+            ham = 1
+            spam = 1
 
-        Returns:
-        - 0 if ham, 1 if spam
-        """
+            comment = self.standardize_data([[comment, -1]])[0]
+            
+            for token in comment[0]:
+                
+                # get both frequencies
 
-        # go through every word and get the probability
-        ham = 1
-        spam = 1
+                # not sure what to do here. If the token isn't in the table, do nothing? need input
+                if token not in self.table:
+                    continue
+                
+                entry = self.table[token]
+                num_ham = entry[0]
+                num_spam = entry[1]
+                
+                # Get the term frequency
+                ham *= (num_ham + 1) / (self.n_ham + 1)
+                spam *= (num_spam + 1) / (self.n_spam + 1)
+                
+            if ham > spam:
+                return 0
+            if spam > ham:
+                return 1
 
-        comment = self.clean_test_string(comment)
+
+     
+    def test_x_comments(self, filepath: str, num_comments: int, use_tf_idf: bool = False) -> None:
+
+        CMS = CommentSanitizer(filepath)
+        comments = CMS.parse()
+        if num_comments > len(comments): 
+            logger.warn(f" number of comments requested ({num_comments}) is greater than list length ({len(comments)})")
+            return
+        comments = comments[:num_comments]
+
+        num_correct = 0
+
+        for comment in comments: 
+
+            content = comment["CONTENT"]
+
+            correct_class = int(comment["CLASS"])
+            if use_tf_idf:
+                guessed_class = self.tf_idf(content)    
+            else:
+                guessed_class = self.predict(content)
+
+            if guessed_class == correct_class:
+                num_correct += 1
         
-        for token in comment.split():
-            
-            # get both frequencies
-
-            # not sure what to do here. If the token isn't in the table, do nothing? need input
-            if token not in self._table:
-                continue
-            
-            entry = self._table[token]
-            num_ham = entry[0]
-            num_spam = entry[1]
-            
-            # if the word is a one-off like [0, 1] or [1, 0], ignore it
-            # maybe just delete them from the table altogether?
-            if entry == [0, 1] or entry == [1, 0]:
-                continue
-
-            # not sure what to do about disproportionate words, or words that appear 0 times in one class and lots in the other, because multiplying a probability by 0 will ruin the process.
-            # for now, I'm going to weight it as 1 / non-zero + 1 
-            if num_ham == 0:
-                
-                ham *= 1 / (num_spam + 1)
-                spam *= 1
-                continue
-            elif num_spam == 0:
-                
-                ham *= 1
-                spam *= 1 / (num_spam + 1)
-                continue
-
-            # if we're all good, calculate
-            ham *= num_ham / (num_ham + num_spam)
-            spam *= num_spam / (num_spam + num_ham)
-            
-        if spam > ham:
-            return 1
-        return 0
-
+        logger.info(f"Number correct: {num_correct}/{num_comments}\t\t{round(num_correct / num_comments, 3)}%")
